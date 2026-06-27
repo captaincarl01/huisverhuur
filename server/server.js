b03cf3c3744cae6e5fc9f234d731dc2ebf578750
Adeyeye Stephen <adeyeyestephen278@gmail.com>
2026-06-27 15:28:16 +0100

    first commit of files

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://huisverhuur.vercel.app",
+    "https://huisverhuuer.vercel.app",
   ],
   credentials: true,
 }));
